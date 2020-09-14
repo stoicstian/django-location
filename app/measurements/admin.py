ebf369fe3e3bd8a3f60051245eeadb89a3b95989
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-09-14 09:55:12 -0500

    Jugando con Model

--- app/measurements/admin.py
+++ app/measurements/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Measurement)
+admin.site.register(Person)
+admin.site.register(Family)
--- app/measurements/models.py
+++ app/measurements/models.py
@@ -1,20 +1,43 @@
 from operator import mod
 from django.db import models
+from django.db.models import CharField
 
 
 # Create your models here.
-class Measurement(models.Model):
-    """Modelo de medidas de un punto a otro."""
+# class Measurement(models.Model):
+#     """Modelo de medidas de un punto a otro."""
 
-    location = models.CharField(max_length=200)
-    destination = models.CharField(max_length=200)
-    distance = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="Distancia en Km"
-    )
+#     location = models.CharField(max_length=200)
+#     destination = models.CharField(max_length=200)
+#     distance = models.DecimalField(
+#         max_digits=10, decimal_places=2, verbose_name="Distancia en Km"
+#     )
+
+#     def __str__(self) -> str:
+#         """Representación clase en formato texto."""
+#         return (
+#             f"Distance from {self.location} "
+#             + "to {self.destination} is {self.distance}"
+#         )
+class Family(models.Model):
+    """Clase familia."""
+
+    nombre = models.CharField("nombre familia", max_length=20)
 
     def __str__(self) -> str:
-        """Representación clase en formato texto."""
-        return (
-            f"Distance from {self.location} "
-            + "to {self.destination} is {self.distance}"
-        )
+        """Método string."""
+        return self.nombre
+
+
+class Person(models.Model):
+    """Clase persona."""
+
+    SexoType = models.TextChoices("Sexo", "Femenino Masculino")
+    nombre = models.CharField("nombre de la persona", max_length=30)
+    apellido = models.CharField("apellido de la persona", max_length=30)
+    familia = models.ForeignKey(Family, on_delete=models.CASCADE, null=True)
+    sexo = models.CharField(blank=True, choices=SexoType.choices, max_length=10)
+
+    def __str__(self) -> str:
+        """Método string."""
+        return f"{self.nombre} {self.apellido}"
