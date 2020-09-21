e019fc946390b0c41daa335c0b26777fd28d1b18
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-09-21 14:05:49 -0500

    Agregando modelo

--- app/config/settings.py
+++ app/config/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "measurements.apps.MeasurementsConfig",
+    # "measurements.apps.MeasurementsConfig",
+    "api.apps.ApiConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
